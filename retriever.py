2cde360327d1ecba9b7c7de91cc256bd2df6c989
sianaisp <anais.plancoulaine@gmail.com>
2025-08-07 09:30:52 +0200

    api key

--- retriever.py
+++ retriever.py
@@ -7,8 +7,10 @@ from loader import load_pdfs_from_folder
 import os
 import streamlit as st
 
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = openai_api_key
 def build_qa_chain():
-    embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["openai_api_key"])
+    embeddings = OpenAIEmbeddings(openai_api_key=openai.api_key
 
     if os.path.exists("faiss_index") and os.path.exists("faiss_index/index.faiss"):
         vector_store = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
